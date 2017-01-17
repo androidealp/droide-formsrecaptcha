7bca690e07c14d3e282b7d7cff9f54aac6b94d36
Andre Luiz <andre@next4.com.br>
2017-01-17 11:36:03 -0200

    ajustes para api dinamica via adm

--- droiderecaptcha.php
+++ droiderecaptcha.php
@@ -6,17 +6,32 @@ defined('_JEXEC') or die ();
 
 include_once (JPATH_ROOT.DS.'plugins'.DS.'droideforms'.DS.'droiderecaptcha'.DS.'lib'.DS.'droide-recaptcha.v2.php');
 
+/**
+ * Plugin para droideforms que utiliza o droide-recaptcha com a v2 da api do google recapchat
+ * @example https://github.com/androidealp/droide-formsrecaptcha - url da extensão
+ * @author André Luiz Pereira <andre@next4.com.br>
+ */
 class plgDroideformsDroiderecaptcha extends JPlugin{
 
   public $erros = null;
   public $recaptcha = '';
     public function __construct(&$subject, $config)
      {
-        $this->recaptcha = new DroideRecaptcha('6LdqpREUAAAAAN4EpqrRZFhe44Mb6FDGanEdSqmR', '6LdqpREUAAAAAHngwL0iUVJjKlE41IGgGFyJ_SP1');
+        $this->recaptcha = new DroideRecaptcha($this->params->get('site_key',''), $this->params->get('secret_key',''));
 
         parent::__construct($subject, $config);
      }
 
+     /**
+      * Metodo para tratar Trigger antes de carregar o layout
+      * @author André Luiz Pereira <andre@next4.com.br>
+      * @param int $id_form - id do formulário
+      * @param string $js - script de chamada para ajax
+      * @param array $params - parametros da extensão
+      * @param array $validacao - parametros da validacao
+      * @param array $custom_vars - variaveis customizadas de outros plugins
+      * @return void
+      */
      public function onDroideformsBeforeLayout(&$id_form, &$js, &$params, &$validacao, &$custom_vars)
      {
        //secret 6LfLohEUAAAAAB6AkDvfy9XN9h_LHIX-7CkhOfYm
@@ -24,6 +39,15 @@ class plgDroideformsDroiderecaptcha extends JPlugin{
        $custom_vars['capcha'] = $this->recaptcha->html();
      }
 
+     /**
+      * Adicione o Trigger para validar o captcha
+      * @author André Luiz Pereira <andre@next4.com.br>
+      * @param array $post - dados vindos pelo post
+      * @param array $validFiltros - parametros da validacao
+      * @param array $errors - array para inserir possiveis erros ocorridos
+      * @param array $custom_vars - variaveis customizadas de outros plugins
+      * @return void
+      */
      public function onDroideformsAddvalidate(&$post, &$validFiltros, &$errors, &$log)
      {
        foreach ($validFiltros['field_name'] as $k => $fild_name) {
@@ -41,12 +65,5 @@ class plgDroideformsDroiderecaptcha extends JPlugin{
        }
 
      }
-
-    //  private function validCaptcha($post,$errors,$validador)
-    //  {
-     //
-    //   return $this->recaptcha->response($post);
-     //
-    //  }
 
 }
